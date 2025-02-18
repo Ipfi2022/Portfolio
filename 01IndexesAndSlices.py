482514f528bf8e7a89f3547a5bb3242d30e2248a
Ipfi2022 <103425163+Ipfi2022@users.noreply.github.com>
2025-02-18 15:42:50 +0200

    Update 01IndexesAndSlices.py

--- 01IndexesAndSlices.py
+++ 01IndexesAndSlices.py
@@ -1,28 +1,28 @@
-ds4_class = ("Mosuli", "KG", "Juza", "Liyema", "Peko", "Sam", "Nota", "Tongo")
-print(ds4_class)
+date_year = (00, 01, 02, 03, 04, 05, 06, 07,08)
+print(date_year)
 
 # Negative indexes are pythonic, but also appear in other languages
-first_student = ds4_class[0]
-print(first_student)
-last_student = ds4_class[-1]
-print(last_student)
-second_student = ds4_class[1]
-print(second_student)
-student_before_last = ds4_class[-2]
-print(student_before_last)
+first_year = date_year[0]
+print(first_year)
+final_year = date_year[-1]
+print(final_year)
+second_year = date_year[1]
+print(second_year)
+year_before_last = date_year[-2]
+print(year_before_last)
 
-print(ds4_class[1:6])
-print(ds4_class[1:])
-print(ds4_class[:6])
+print(date_year[1:6])
+print(date_year[1:])
+print(date_year[:6])
 
-print(ds4_class[1:6:2])
+print(date_yeat[1:6:2])
 
 slice1 = slice(1, 6, 2)
-print(ds4_class[slice1])
+print(date_year[slice1])
 slice2 = slice(1,6)
-print(ds4_class[slice2])
+print(date_year[slice2])
 
-# ds4_class.add("Zuma"); error because tuples like ds4_class are immutable
+# date_year.add(04); error because tuples like ds4_class are immutable
 
 for student in ds4_class:
     print(student)
