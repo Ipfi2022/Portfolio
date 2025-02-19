e778194c029b1128d41f4cd7969efdb7a430879f
Ipfi2022 <103425163+Ipfi2022@users.noreply.github.com>
2025-02-19 10:10:32 +0200

    Add files via upload

---
+++ 02UserDefinedSequences.py
@@ -0,0 +1,30 @@
+from collections.abc import Sequence
+
+class Teams(Sequence):
+    def __init__(self, values, name="DefaultSequence"):
+        self.values = list(values)
+        self.name = name
+        self.length = len(values)
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        return self.values[index]
+
+    def __repr__(self):
+        return f"{self.name}({self.values})"
+
+teams1 = Teams(("Pirates", "Chiefs"), name="Mzanzi teams")
+teams2 = Teams(("United", "City"), name="English Premier League Teams")
+teams3 = Teams(("Barca", "Real"), name="LaLiga Teams")
+
+
+print(teams1)
+print(teams2)
+print(teams3)
+
+print(teams2[0])
+
+for team in teams1:
+    print(team)
